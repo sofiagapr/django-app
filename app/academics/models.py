48b3bf1c7eb50c13e47811a653f42c2ec2083eb2
sofiagapr <zofiagapr@gmail.com>
2024-03-18 10:54:13 -0500

    new

--- app/academics/models.py
+++ app/academics/models.py
@@ -1,19 +1,70 @@
 from django.db import models
 import datetime
 # Create your models here.
+#model.DateTimeField(auto_now = true, null = false ) => update_at
+#model.DateTimeField(auto_now_add = true, null = false ) => Defaul now()
+#usamos models.Model para usar los timpos de campos ejemplos variables
 class User(models.Model):
-    email= models.EmailField(null=True, blank= True)
-    password = models.CharField(null=True, blank= True)
-    status = models.BooleanField(default= True)
-    created_at = models.DateTimeField(default=datetime.datetime.now())
-    updated_at = models.DateTimeField(default=datetime.datetime.now())
-    deleted_at = models.DateTimeField(null=True, blank= True)
+    email = models.EmailField(null = True, blank = True)
+    password = models.CharField(null = True, blank = True)
+    status = models.BooleanField(null = True, blank = True, default = True)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
 
-class Person (models.Model):
-    firstname = models.CharField(max_length = 20)
+class Person(models.Model):
+    firstname = models.CharField(max_length = 20) 
     lastname = models.CharField(max_length = 20)
     age = models.IntegerField()
-    ident_number= models.DateTimeField(max_length = 12, blank=True)
-    created_at = models.DateTimeField(default=datetime.datetime.now())
-    updated_at = models.DateTimeField(default=datetime.datetime.now())
-    deleted_at = models.DateTimeField(null=True, blank= True)
+    ident_number = models.CharField(max_length = 12, blank = True)
+
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+
+class Student(models.Model):
+    code = models.CharField(max_length = 50) 
+    id_person = models.IntegerField()
+    status = models.BooleanField(null = True, blank = True, default = True) 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+
+class Identification_type(models.Model):
+    name = models.CharField(max_length = 50)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 100) 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+
+class City(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
+    id_dept = models.IntegerField() 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+
+class Departament(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
+    id_country = models.IntegerField() 
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length = 100)
+    abrev = models.CharField(max_length = 10)  
+    descrip = models.CharField(max_length = 10)
+    created_at = models.DateTimeField(default = datetime.datetime.now())
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +80,11 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'app',
-        'USER': 'postgres',
-        'PASSWORD': 'unicesmag',
-        'HOST': 'localhost',
-        'PUERTO': '5432',
+        'NAME': config ('DB_NAME'),
+        'USER': config ('DB_USER'),
+        'PASSWORD': config ('DB_PASSWORD'),
+        'HOST': config ('DB_HOST'),
+        'PUERTO': config ('DB_PORT'),
     }
 }
 
