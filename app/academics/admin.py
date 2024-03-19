09b4ef38d8881848d3e8968800cdd7d301728e60
sofiagapr <zofiagapr@gmail.com>
2024-03-19 12:05:23 -0500

    commit 19/03

--- app/academics/admin.py
+++ app/academics/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from.models import User, Person, Student, Identification_type, City, Department, Country
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Person)
+admin.site.register(Student)
+admin.site.register(Identification_type)
+admin.site.register(City)
+admin.site.register(Department)
+admin.site.register(Country)
--- app/academics/models.py
+++ app/academics/models.py
@@ -12,6 +12,9 @@ class User(models.Model):
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
 
+    def __str__(self):
+        return self.email
+
 class Person (models.Model):
     firstname = models.CharField(max_length = 20) 
     lastname = models.CharField(max_length = 20)
@@ -25,7 +28,7 @@ class Person (models.Model):
     updated_at = models.DateTimeField(default = datetime.datetime.now())
     deleted_at = models.DateTimeField(null = True, blank = True)
     
-class Students(models.Model):
+class Student(models.Model):
     code = models.CharField(max_length = 20)
     status = models.BooleanField(null = True, blank = True, default = True)
     created_at = models.DateTimeField(default = datetime.datetime.now())
